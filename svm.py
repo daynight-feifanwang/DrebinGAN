323a5714857ee428c36a9094a51372eb47f36268
daynight-feifanwang <feifan.wang@hotmail.com>
2021-08-23 19:48:36 +0800

    add pre feature selector

--- svm.py
+++ svm.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import LinearSVC, SVC
 from sklearn.metrics import accuracy_score, f1_score, classification_report
-from sklearn.feature_selection import RFECV
+from sklearn.feature_selection import GenericUnivariateSelect, SelectFromModel, chi2
 
 import utils
 
@@ -17,35 +17,33 @@ logger.setLevel("INFO")
 
 
 class DrebinSVM(object):
-    def __init__(self, load_path='./updated_data', save_path='./save_model', train_size=0.7):
+    def __init__(self, load_path='./middle_data', save_path ='./middle_data', model_path='./save_model', train_size=0.7):
         self.load_path = utils.get_absolute_path(load_path)
         self.save_path = utils.get_absolute_path(save_path)
+        self.model_path = utils.get_absolute_path(model_path)
         self.train_size = train_size
 
         self.raw_data_path = utils.get_absolute_path('./raw_data')
-        self.updated_data_path = utils.get_absolute_path('./updated_data')
         self.raw_samples = 'apgx.json'
         self.raw_labels = 'apgy.json'
-        self.clean_threshold = 3
-        self.url_allowed = True
 
         self.model = None
-        self.mlb = None
+        self.mlb = MultiLabelBinarizer(sparse_output=True)
 
     def train(self):
         ###### collect samples #####
         logger.info("Loading data for DrebinSVM...")
 
         samples, labels, features = self.load_feature()
+        feature_list = [[feature] for feature in features]
 
         logger.info("Loading Done.")
 
         ##### generate feature space #####
         logger.info("Generating feature space...")
 
-        self.mlb = MultiLabelBinarizer(sparse_output=True)
-
-        x = self.mlb.fit_transform(samples)
+        self.mlb.fit(feature_list)
+        x = self.mlb.transform(samples)
         y = np.array(labels)
         # malware will be marked as 1 otherwise will be marked as -1
 
@@ -62,13 +60,13 @@ class DrebinSVM(object):
         logger.info("Start training classifier with SVM...")
 
         # ready precondition
-        parameters = [{'C': [0.01, 0.05, 0.1, 0.25, 0.5, 1, 5, 10, 100],
-                       # 'kernel': ['linear', 'rbf', 'sigmoid', 'poly'],
-                       'kernel': ['linear'],
-                       'gamma': [100, 10, 1, 0.1, 0.01, 0.001],
-                       'degree': [2, 3, 4, 5]}]
-        # classifier = GridSearchCV(LinearSVC(loss='hinge'), parameters, cv=5, scoring='f1', n_jobs=-1, verbose=2)
-        self.model = GridSearchCV(SVC(probability=True), parameters, cv=5, scoring='accuracy', n_jobs=-1, verbose=5)
+        parameters = [{
+            'C': [0.01, 0.05, 0.1, 0.25, 0.5, 1, 5, 10, 100],
+            'penalty': ['l1', 'l2'],
+            'loss': ['hinge', 'squared_hinge']
+        }]
+        self.model = GridSearchCV(LinearSVC(max_iter=5000), parameters, cv=5, scoring='f1', n_jobs=-1, verbose=2, error_score=0.0)
+        # self.model = GridSearchCV(SVC(probability=True), parameters, cv=5, scoring='accuracy', n_jobs=-1, verbose=5)
 
         # train phrase
         self.model.fit(x_train, y_train)
@@ -116,21 +114,18 @@ class DrebinSVM(object):
         return np.array(y), np.array(z)
 
     def load(self):
-        self.model = joblib.load(os.path.join(self.save_path, 'svm_model.pkl'))
+        self.model = joblib.load(os.path.join(self.model_path, 'svm_model.pkl'))
 
-        feature_list = utils.import_from_pkl(self.updated_data_path, 'total_feature_list.data')
+        feature_list = utils.import_from_pkl(self.load_path, 'total_feature_list.data')
         feature_list = [[feature] for feature in feature_list]
 
         self.mlb = MultiLabelBinarizer(sparse_output=True)
         self.mlb.fit(feature_list)
 
     def save(self, classifier):
-        if not os.path.exists(self.save_path):
-            os.makedirs(self.save_path)
-        joblib.dump(classifier, os.path.join(self.save_path, 'svm_model.pkl'))
-
-    def transform(self, x):
-        return x
+        if not os.path.exists(self.model_path):
+            os.makedirs(self.model_path)
+        joblib.dump(classifier, os.path.join(self.model_path, 'svm_model.pkl'))
 
     def load_data(self, x):
         if type(x) is str:
@@ -144,14 +139,17 @@ class DrebinSVM(object):
 
     def load_feature(self):
         load_abs_path = utils.get_absolute_path(self.load_path)
+        if not os.path.exists(load_abs_path):
+            os.makedirs(load_abs_path)
+
         logger.info("Loading Data...")
         if 'sample_list.data' in os.listdir(load_abs_path):
             samples = utils.import_from_pkl(load_abs_path, 'sample_list.data')
         else:
             # Extract samples' features from raw data.
             samples = utils.import_from_json(self.raw_data_path, self.raw_samples)
-            samples, updated_features = self.clean_data(samples)  # Format samples with clean[optional].
-            utils.export_to_pkl(self.updated_data_path, 'sample_list.data', samples)
+            samples = [list(sample.keys()) for sample in samples]
+            utils.export_to_pkl(self.save_path, 'sample_list.data', samples)
         logger.info("Loading samples DONE.")
 
         if 'label_list.data' in os.listdir(load_abs_path):
@@ -160,84 +158,47 @@ class DrebinSVM(object):
             # Extract samples' labels from raw data.
             labels = utils.import_from_json(self.raw_data_path, self.raw_labels)
             labels = [1 if label == 1 else -1 for label in labels]
-            utils.export_to_pkl(self.updated_data_path, 'label_list.data', labels)
+            utils.export_to_pkl(self.save_path, 'label_list.data', labels)
         logger.info("Loading labels DONE.")
 
-        if 'total_feature_list.data' in os.listdir(load_abs_path):
-            features = utils.import_from_pkl(load_abs_path, 'total_feature_list.data')
-            if 'updated_features' in dir():
-                features = sorted(updated_features)
-                utils.export_to_pkl(self.updated_data_path, 'total_feature_list.data', features)
-
+        if 'feature_list.data' in os.listdir(load_abs_path):
+            features = utils.import_from_pkl(load_abs_path, 'feature_list.data')
         else:
             # Extract using features from samples.
-            features = sorted(updated_features)
-            utils.export_to_pkl(self.updated_data_path, 'total_feature_list.data', features)
+            features = [feature for sample in samples for feature in sample]
+            features = sorted(list(set(features)))
+            utils.export_to_pkl(self.save_path, 'feature_list.data', features)
         logger.info("Loading Feature vocabulary Done.")
 
         return samples, labels, features
 
-    def clean_data(self, samples):
-        # url_allowed == True -> ignore threshold for url
-        # raw sample type: list[dict[str, int]] -> samples[sample[feature, 1]]
-        try:
-            samples = [list(sample.keys()) for sample in samples]
-        except Exception as e:
-            logger.debug("Formatting raw features FAILED with exception {}.".format(e))
-        data = [features for sample in samples for features in sample]
-        features_count = Counter(data)
-        features = set()
-        for feat, count in features_count.items():
-            if self.url_allowed and feat.startswith('urls'):
-                features.add(feat)
-            elif count >= self.clean_threshold:
-                features.add(feat)
-            else:
-                continue
+    def clean_data(self, samples, features):
+        # clean samples data with selected features.
+        feature_set = set(features)
         for i_s, sample in enumerate(samples):
-            samples[i_s] = [feature for feature in sample if feature in features]
+            samples[i_s] = [feature for feature in sample if feature in feature_set]
 
-        return samples, list(features)
+        utils.export_to_pkl(self.save_path, 'sample_list.data', samples)
 
-    def test(self):
-        """
-            x_real = utils.import_from_pkl('../updated_data/sample_list.data')
-            y_real = utils.import_from_pkl('../updated_data/label_list.data')
-            os.rename('../updated_data/sample_list.data', '../updated_data/sample_list.data_')
-            os.rename('../updated_data/label_list.data', '../updated_data/label_list.data_')
+    def pre_select_feature(self, param, clean_sample=False):
+        # load features
+        samples, labels, features = self.load_feature()
 
-            y_pred, z_pred = c.predict('./updated_data/sample_list.data_')
+        # transform samples to sparse matrix
+        x = self.mlb.fit_transform(samples)
+        y = np.array(labels)
 
-            x_updated = [x for i, x in enumerate(x_real) if y_real[i] == 1]
-            y_updated = [1 for i in range(len(x_updated))]
+        # select features
+        selector = GenericUnivariateSelect(chi2, mode='percentile', param=param)
+        x_new = selector.fit_transform(x, y)
 
-            x_good = []
-            for i, _ in enumerate(x_real):
-                if y_real[i] == -1 and y_pred[i] == -1:
-                    tmp = x_real[i]
-                    tmp.append(z_pred[i][0] - z_pred[i][1])
-                    x_good.append(tmp)
-            x_good.sort(key=lambda x: x[-1])
-            x_good = [x[:-1] for x in x_good]
-            x_updated += x_good[:round(len(x_good) * 0.6)]
+        # map features
+        mask = selector.get_support(True)
+        features = list(np.array(features)[mask])
+        utils.export_to_pkl(self.save_path, 'feature_list.data', features)
 
-            y_updated += [-1 for i in range(round(len(x_good) * 0.6))]
-
-            utils.export_to_pkl('../updated_data/sample_list.data', Content=x_updated)
-            utils.export_to_pkl('../updated_data/label_list.data', Content=y_updated)
-
-            c.train()
-
-            x = c.mlb.transform(x_real)
-            y = y_real
-            c.evaluate(x, y)
-
-            os.remove('../updated_data/sample_list.data')
-            os.remove('../updated_data/label_list.data')
-            os.rename('../updated_data/sample_list.data_', '../updated_data/sample_list.data')
-            os.rename('../updated_data/label_list.data_', '../updated_data/label_list.data')
-            """
-        pass
+        if clean_sample is True:
+            self.clean_data(samples, features)
 
     def select_feature(self):
         # load model
@@ -247,19 +208,25 @@ class DrebinSVM(object):
         # load data
         samples, labels, _ = self.load_feature()
 
+        ### test ###
+        selector = SelectFromModel(self.model.best_estimator_, prefit=True)
+        x_new = selector.transform(samples)
+
         x = self.mlb.transform(samples)
         y = np.array(labels)
 
         # select features
-        rfecv = RFECV(self.model.best_estimator_, cv=5, scoring='accuracy', verbose=3)
-        rfecv.fit(x, y)
+        # rfecv = RFECV(self.model.best_estimator_, cv=5, scoring='accuracy', n_jobs=-1, verbose=3)
+        # rfecv.fit(x, y)
 
-        print(1)
+        # print(rfecv.n_features_to_select)
+        # print(rfecv.ranking_)
 
 
 if __name__ == '__main__':
     c = DrebinSVM()
+    c.pre_select_feature(20)
     c.train()
-    c.select_feature()
+    # c.select_feature()
 
 
